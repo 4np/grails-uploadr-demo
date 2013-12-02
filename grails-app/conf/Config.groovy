2117a28f17b26ee38c5fede409c4ace6b2f9b6d1
Jeroen Wesbeek <work@osx.eu>
2013-12-02 22:41:06 +0100

    added uploadr plugin and demo view

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -113,3 +113,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// used by the demo tag
+uploadr.defaultUploadPath="${System.getProperty('user.home')}/Downloads/uploadr"
+// used by the cleanUploadedFilesJob Quartz job (on ci and development)
+uploadr.maxAgeUploadedFile=1000 * 60  * 60  * 4  // 4 hours in milliseconds
---
+++ grails-app/views/demo.gsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <r:require modules="uploadr"/>
+    <r:layoutResources/>
+</head>
+<body>
+<uploadr:demo/>
+<r:layoutResources/>
+</body>
+</html>
